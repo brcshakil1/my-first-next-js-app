cdebb5b0dcb23663e2918f4dd902e1fccb125a57
brcshakil1 <brcshakil72@gmail.com>
2023-12-01 17:03:10 +0600

    created a client component by using 'use client' on the component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Counter from "@/components/Counter/Counter";
 import React from "react";
 
 const HomePage = () => {
@@ -6,6 +7,9 @@ const HomePage = () => {
       <h1 className="text-center text-2xl pt-10 font-bold">
         My first next js app
       </h1>
+      <div>
+        <Counter />
+      </div>
     </div>
   );
 };
