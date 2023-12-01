18e0698d517ecad3045e1335e7ed242fe9164496
brcshakil1 <brcshakil72@gmail.com>
2023-12-01 12:45:46 +0600

    first commit or my first next js app

---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h2>Moulvibazar, Sylhet, Bangladesh</h2>
+    </div>
+  );
+};
+
+export default ContactPage;
